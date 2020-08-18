fa57aec1c90a39371f07d315d2b7e0648eec771e
theLastRefugee <59699800+YanjieZe@users.noreply.github.com>
2020-08-18 22:03:08 +0800

    Add files via upload

--- 第3章 k近邻法/kdtree.py
+++ 第3章 k近邻法/kdtree.py
@@ -85,73 +85,73 @@ class kdtree(object):
             print(root.data)
         backOrder(self.root)
 
+
 result = namedtuple("Result_tuple",
                     "nearest_point  nearest_dist  nodes_visited")
+
 
 def find_nearest(tree, point):
     k = len(point)
 
     def travel(kd_node, target, max_dist):
         if kd_node is None:
-            return result([0] * k, float("inf"),0)  # python中用float("inf")和float("-inf")表示正负无穷
-
+            return result([0]*k, float("inf"), 0)
         nodes_visited = 1
 
-        s = kd_node.split  # 进行分割的维度
-        pivot = kd_node.dom_elt  # 进行分割的“轴”
+        s = kd_node.split
+        pivot = kd_node.data
 
-        if target[s] <= pivot[s]:  # 如果目标点第s维小于分割轴的对应值(目标离左子树更近)
-            nearer_node = kd_node.left  # 下一个访问节点为左子树根节点
-            further_node = kd_node.right  # 同时记录下右子树
-        else:  # 目标离右子树更近
-            nearer_node = kd_node.right  # 下一个访问节点为右子树根节点
+        if target[s] <= pivot[s]:
+            nearer_node = kd_node.left
+            futher_node = kd_node.right
+        else:
+            nearer_node = kd_node.right
             further_node = kd_node.left
 
-        temp1 = travel(nearer_node, target, max_dist)  # 进行遍历找到包含目标点的区域
-
-        nearest = temp1.nearest_point  # 以此叶结点作为“当前最近点”
-        dist = temp1.nearest_dist  # 更新最近距离
+        temp1 = travel(nearer_node, target, max_dist)
+        nearest = temp1.nearest_point
+        dist = temp1.nearest_dist
 
         nodes_visited += temp1.nodes_visited
 
         if dist < max_dist:
-            max_dist = dist  # 最近点将在以目标点为球心，max_dist为半径的超球体内
+            max_dist = dist
 
-        temp_dist = abs(pivot[s] - target[s])  # 第s维上目标点与分割超平面的距离
-        if max_dist < temp_dist:  # 判断超球体是否与超平面相交
-            return result(nearest, dist, nodes_visited)  # 不相交则可以直接返回，不用继续判断
+        temp_dist = abs(pivot[s] - target[s])
+        if max_dist < temp_dist:
+            return result(nearest, dist, nodes_visited)
 
-        # ----------------------------------------------------------------------
-        # 计算目标点与分割点的欧氏距离
         temp_dist = sqrt(sum((p1 - p2) ** 2 for p1, p2 in zip(pivot, target)))
 
-        if temp_dist < dist:  # 如果“更近”
-            nearest = pivot  # 更新最近点
-            dist = temp_dist  # 更新最近距离
-            max_dist = dist  # 更新超球体半径
+        if temp_dist < dist:
+            nearest = pivot
+            dist = temp_dist
+            max_dist = dist
 
-        # 检查另一个子结点对应的区域是否有更近的点
         temp2 = travel(further_node, target, max_dist)
 
         nodes_visited += temp2.nodes_visited
-        if temp2.nearest_dist < dist:  # 如果另一个子结点内存在更近距离
-            nearest = temp2.nearest_point  # 更新最近点
-            dist = temp2.nearest_dist  # 更新最近距离
+        if temp2.nearest_dist < dist:
+            nearest = temp2.nearest_point
+            dist = temp2.nearest_dist
 
         return result(nearest, dist, nodes_visited)
 
-    return travel(tree.root, point, float("inf"))  # 从根节点开始递归
+    return travel(tree.root, point, float("inf"))
 
 
 
-if __name__ == '__main__':
-    # print(lpdistance([2,2],[1,1],2))
-    dataset = [[2,3],[5,4],[9,6],[4,7],[8,1],[7,2]]
-    tree = kdtree(dataset=dataset)
-    print("前序遍历：")
-    tree.preOrder()
-    print("中序遍历：")
-    tree.midOrder()
-    print("后序遍历：")
-    tree.backOrder()
+# print(lpdistance([2,2],[1,1],2))
+dataset = [[2,3],[5,4],[9,6],[4,7],[8,1],[7,2]]
+tree = kdtree(dataset=dataset)
+print("前序遍历：")
+tree.preOrder()
+print("中序遍历：")
+tree.midOrder()
+print("后序遍历：")
+tree.backOrder()
+
+res = find_nearest(tree=tree,point=[2,3])
+print(res.nearest_dist)
+print(res.nearest_point)
 
